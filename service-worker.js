e27a867bd96c89c216a4527466b582255cdbebd7
maw4241-ctrl <maw4241@gmail.com>
2026-05-21 08:24:42 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,1 @@
-const CACHE_NAME = 'well-done-pro-v1';
-const ASSETS = ['./', './index.html', './manifest.json'];
 
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(() => null));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null))));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request).then(res => res || caches.match('./index.html'))));
-});
